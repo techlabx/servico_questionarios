13f9a18a9720d505542868bc1daadfe5cadecc40
Giovana Daniele <48071009+giovanadanieles@users.noreply.github.com>
2020-05-22 00:16:59 -0300

    Atualizando script do BD para versão mais recente.

--- smBD.sql
+++ smBD.sql
@@ -28,9 +28,9 @@ CREATE TABLE direcionamento(
 
 
 
-INSERT INTO questionario VALUES ('SRQ-20', 'Teste que avalia o sofrimento mental. As questões são relacionadas a certas dores e problemas que podem ter lhe incomodado nos últimos 30 dias.');
+INSERT INTO questionario VALUES ('SRQ-20', 'Nas próximas próximas perguntas, você realizará uma autoavaliação a respeito da sua saúde em geral. Serão 24 afirmativas, e você deve selecionar sim ou não para cada afirmativa. Não existe resposta certa ou errada, por isso pedimos que você responda da forma que você julgar melhor. Caso tenha alguma dúvida, você pode nos mandar uma mensagem pelo XXX.');
 								  
-INSERT INTO questionario VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Blabla');
+INSERT INTO questionario VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Nessa escala, será avaliado o risco de suicídio através de algumas perguntas. Essa escala foi criada por pesquisadores de diversas universidades (Universidade de Columbia; Universidade da Pensilvânia; Universidade de Pittsburgh e Universidade de Nova Iorque) e avalia comportamentos específicos que podem ser indicativos da intenções de uma pessoa em cometer suicídio. É importante ressaltar que os resultados apresentados são referentes a um período específico de sua vida, e que também podem mudar de acordo com as condições ambientais e temporais. É importante destacar que este resultado não visa definir você enquanto “suicida”, mas, sim, mostrar a necessidade de você procurar ajuda para enfrentar as dificuldades que está passando, no momento. Por isso pedimos que você seja o mais sincero/a possível nas respostas, para que você saiba se é importante buscar ajuda, ou de familiares e amigos, ou de uma equipe de saúde especializada.');
 
 
 
