56d4538fadc07614efb8a373b5514d25676f1768
Leonardo Prati <leonardo@sflabs.com.br>
2020-06-19 15:30:48 -0300

    debug prints

--- db/database.js
+++ db/database.js
@@ -9,7 +9,7 @@ const questionarios = require("../common/questionarios");
 bluebird.promisifyAll(redis);
 const cache = redis.createClient(6379, "redis");
 
-cache.on("error", function(error) {
+cache.on("error", function (error) {
   console.error(error);
 });
 
@@ -21,20 +21,20 @@ initCache();
 
 // =================  Questionários =================
 //Lista os questionários
-async function listarQuestionarios (req, res) {
+async function listarQuestionarios(req, res) {
   try {
     // Função que pega os questionário do BD
     let listagem = await queries.getAllQuest();
     res.send(listagem);
   } catch (err) {
     console.log(err);
-    res.status(400).json({error: 'Erro ao listar questionário. Consulte o console do server para mais detalhes'});
+    res.status(400).json({ error: 'Erro ao listar questionário. Consulte o console do server para mais detalhes' });
   }
 
 }
 
 //Cria no BD um questionário
-async function criarQuestionario (req, res) {
+async function criarQuestionario(req, res) {
   try {
     //Precisamos discutir o formato, pode ser um JSON com o nome do questionário:
     // {
@@ -48,19 +48,19 @@ async function criarQuestionario (req, res) {
     let resultado = queries.createQuest(questionario);
     res.send(resultado);
   } catch (err) {
-    res.status(400).json({error: 'Erro ao criar questionário. Consulte o console do server para mais detalhes'})
+    res.status(400).json({ error: 'Erro ao criar questionário. Consulte o console do server para mais detalhes' })
   }
 }
 
 // =================  Questões =================
 //Lista as questõe de um questionário dado um id/nome
-async function listarQuestoes (req, res) {
+async function listarQuestoes(req, res) {
   try {
     var questionario = req.body.nome;
     let perguntas = await queries.getAllPerg(questionario);
     res.send(perguntas);
   } catch (err) {
-    res.status(400).json({error: 'Erro ao listar questoes do questionário. Consulte o console do server para mais detalhes'})
+    res.status(400).json({ error: 'Erro ao listar questoes do questionário. Consulte o console do server para mais detalhes' })
   }
 }
 
@@ -107,10 +107,10 @@ async function iniciarQuestionario(req, res) {
     //Isso aqui pode dar problema caso o questionário não seja encontrado, né?
     //Como podemos contornar isso?
     let tcle = ["Olá!", `A seguir, você irá preencher o questionário ${nome_questionario}.`,
-    "Antes de iniciá-lo, gostaríamos de saber se você teria interesse em enviar suas respostas diretamente para o banco de dados o APOIA USP.",
-    "Queremos esclarecer que esses dados serão sigilosos e restritos aos funcionário do serviço.",
-    "Se você desejar compartilhar seus dados entenderemos que, caso haja necessidade, o serviço poderá entrar em contato com você através do seu e-mail USP fornecido na inscrição.",
-    "Se você desejar NÃO compartilhar seus dados, isso não trará nenhum prejuízo para você, contudo, não conseguiremos entrar em contato para oferecer nosso serviço de Atenção Psicossocial."];
+      "Antes de iniciá-lo, gostaríamos de saber se você teria interesse em enviar suas respostas diretamente para o banco de dados o APOIA USP.",
+      "Queremos esclarecer que esses dados serão sigilosos e restritos aos funcionário do serviço.",
+      "Se você desejar compartilhar seus dados entenderemos que, caso haja necessidade, o serviço poderá entrar em contato com você através do seu e-mail USP fornecido na inscrição.",
+      "Se você desejar NÃO compartilhar seus dados, isso não trará nenhum prejuízo para você, contudo, não conseguiremos entrar em contato para oferecer nosso serviço de Atenção Psicossocial."];
 
     console.log(tcle);
 
@@ -118,18 +118,18 @@ async function iniciarQuestionario(req, res) {
     await cache.set(`${nome_questionario}_${new_id}`, ESSerializer.serialize(q));
 
     res.status(200).send({
-        session_id: new_id,
-        intro: tcle
+      session_id: new_id,
+      intro: tcle
     });
 
   } catch (err) {
     console.log(err);
-    res.status(400).json({ error: 'Erro ao iniciar questionário. Consulte o console do server para mais detalhes'});
+    res.status(400).json({ error: 'Erro ao iniciar questionário. Consulte o console do server para mais detalhes' });
   }
 }
 
 //Recebe a resposta e passa a proxima questão
-async function proximaQuestao (req, res) {
+async function proximaQuestao(req, res) {
   try {
     var questionario = req.params.name;
     var session_id = req.params.session_id;
@@ -140,18 +140,19 @@ async function proximaQuestao (req, res) {
     q = ESSerializer.deserialize(q, [questionarios.Columbia, questionarios.SRQ20]);
 
     let prox_pergunta = "";
-    if(resposta == "li e aceito" ||
-       resposta == "li e nao aceito"){
+    if (resposta == "li e aceito" ||
+      resposta == "li e nao aceito") {
 
-        if(resposta == "li e aceito") {
-          q.setCompartilhamento(true);
-        }
+      if (resposta == "li e aceito") {
+        q.setCompartilhamento(true);
+      }
 
-        prox_pergunta = q.leProximaPergunta();
+      prox_pergunta = q.leProximaPergunta();
     }
     else {
       if (resposta != "sim" && resposta != "nao") {
         prox_pergunta = q.leProximaPergunta();
+        console.log("returning at 155");
         res.status(200).send({
           session_id: session_id,
           question: prox_pergunta,
@@ -173,13 +174,14 @@ async function proximaQuestao (req, res) {
     if (prox_pergunta == "Fim do questionário") {
       ultima_mensagem = true;
       resultado = q.calculaResultado();
-      if(q.getCompartilhamento()) await sendMail(questionario, q, resultado[0]);
+      if (q.getCompartilhamento()) await sendMail(questionario, q, resultado[0]);
     }
 
     if (!ultima_mensagem) {
       // Atualizar cache
       await cache.set(`${questionario}_${session_id}`, ESSerializer.serialize(q));
 
+      console.log("returning at 184");
       res.status(200).send({
         session_id: session_id,
         question: prox_pergunta,
@@ -188,6 +190,7 @@ async function proximaQuestao (req, res) {
       });
     } else {
 
+      console.log("returning at 193");  
       // Remover do cache
       res.status(200).send({
         session_id: session_id,
@@ -198,33 +201,34 @@ async function proximaQuestao (req, res) {
 
   } catch (err) {
     console.log(err);
-    res.status(400).json({ error: 'Erro ao pedir próxima questão. Consulte o console do server para mais detalhes'});
+    console.log("returning at 204");
+    res.status(400).json({ error: 'Erro ao pedir próxima questão. Consulte o console do server para mais detalhes' });
   }
 
 }
 
 async function sendMail(nome_questionario, obj_questionario, result_quest) {
 
-    payload = {
-      "usuario": {
-        "nome": "Usuario nao identificado",
-        "nusp": "000000000"
-      },
-      "questionario": {
-        "nome": nome_questionario,
-        "respostas": {}
+  payload = {
+    "usuario": {
+      "nome": "Usuario nao identificado",
+      "nusp": "000000000"
     },
-      "resultado": result_quest
-    }
+    "questionario": {
+      "nome": nome_questionario,
+      "respostas": {}
+    },
+    "resultado": result_quest
+  }
 
-    let i;
-    for (i = 1; i <= Object.keys(obj_questionario.respostas).length; i++) {
-      payload.questionario.respostas[i] = {"pergunta": "", "resposta": ""};
-      payload.questionario.respostas[i].pergunta = obj_questionario.perguntas[i];
-      payload.questionario.respostas[i].resposta = obj_questionario.respostas[i];
-    }
+  let i;
+  for (i = 1; i <= Object.keys(obj_questionario.respostas).length; i++) {
+    payload.questionario.respostas[i] = { "pergunta": "", "resposta": "" };
+    payload.questionario.respostas[i].pergunta = obj_questionario.perguntas[i];
+    payload.questionario.respostas[i].resposta = obj_questionario.respostas[i];
+  }
 
-    await axios.post('http://servico_email:8080/email/relatorio/enviar', payload)
+  await axios.post('http://servico_email:8080/email/relatorio/enviar', payload)
     .then(function (response) {
       console.log(response);
     }).catch(function (error) {
